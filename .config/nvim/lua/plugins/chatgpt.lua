e3a3606b1ade824fa9575298a6c86cf3b2ac1a23
Emilprivate <emilsprojects@gmail.com>
2024-03-30 02:43:42 +0100

    local changes

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -1,17 +1,124 @@
 -- NOTE: ChatGPT Plugin --> Require API key
 return {
-	{
-		"jackMort/ChatGPT.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			require("chatgpt").setup({
-				api_key_cmd = "pass show openai", -- OpenAI API Key
-			})
-		end,
-	},
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("chatgpt").setup {
+        api_key_cmd = "pass show openai", -- OpenAI API Key
+        openai_params = {
+          model = "gpt-4-0125-preview", -- Update the model here
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 300,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        openai_edit_params = {
+          model = "gpt-4-0125-preview",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        chat = {
+
+          loading_text = "Loading, please wait ...",
+          question_sign = "", -- 🙂
+          answer_sign = "ﮧ", -- 🤖
+          max_line_length = 120,
+          sessions_window = {
+            border = {
+              style = "rounded",
+              text = {
+                top = " Sessions ",
+              },
+            },
+            win_options = {
+              winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+            },
+          },
+        },
+        popup_layout = {
+          default = "center",
+          center = {
+            width = "80%",
+            height = "80%",
+          },
+          right = {
+            width = "30%",
+            width_settings_open = "50%",
+          },
+        },
+        popup_window = {
+          border = {
+            highlight = "FloatBorder",
+            style = "rounded",
+            text = {
+              top = " ChatGPT ",
+            },
+          },
+          win_options = {
+            wrap = true,
+            linebreak = true,
+            foldcolumn = "1",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+          },
+          buf_options = {
+            filetype = "markdown",
+          },
+        },
+        system_window = {
+          border = {
+            highlight = "FloatBorder",
+            style = "rounded",
+            text = {
+              top = " SYSTEM ",
+            },
+          },
+          win_options = {
+            wrap = true,
+            linebreak = true,
+            foldcolumn = "2",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+          },
+        },
+        popup_input = {
+          prompt = "  ",
+          border = {
+            highlight = "FloatBorder",
+            style = "rounded",
+            text = {
+              top_align = "center",
+              top = " Prompt ",
+            },
+          },
+          win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+          },
+          submit = "<C-Enter>",
+          submit_n = "<Enter>",
+          max_visible_lines = 20,
+        },
+        settings_window = {
+          border = {
+            style = "rounded",
+            text = {
+              top = " Settings ",
+            },
+          },
+          win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+          },
+        },
+      }
+    end,
+  },
 }
